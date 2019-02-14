2fa1becb69cf4ab9d3ddc9ca243968268871c255
Chase <chase@mailbox.org>
2019-02-14 23:13:32 +0100

    Edited SensorTest to print picoFarad as json

--- src/SensorTest/SensorTest.ino
+++ src/SensorTest/SensorTest.ino
@@ -44,25 +44,14 @@ void loop(){
   }
 
   elapsedTime= millis() - startTime;
- // convert milliseconds to seconds ( 10^-3 ) and Farads to microFarads ( 10^6 ),  net 10^3 (1000)  
-  microFarads = ((float)elapsedTime / resistorValue) * 1000;   
-  Serial.print(elapsedTime);       // print the value to serial port
-  Serial.print(" mS    ");         // print units and carriage return
+ // convert milliseconds to seconds ( 10^-3 ) and Farads to picoFarads ( 10^12 ),  net 10^9 (1000000000)  
+  picoFarads = ((float)elapsedTime / resistorValue) * 1000000000;   
 
-
-  if (microFarads > 1){
-    Serial.print((long)microFarads);       // print the value to serial port
-    Serial.println(" microFarads");         // print units and carriage return
-  }
-  else
-  {
-    // if value is smaller than one microFarad, convert to nanoFarads (10^-9 Farad). 
-    // This is  a workaround because Serial.print will not print floats
-
-    nanoFarads = microFarads * 1000.0;      // multiply by 1000 to convert to nanoFarads (10^-9 Farads)
-    Serial.print((long)nanoFarads);         // print the value to serial port
-    Serial.println(" nanoFarads");          // print units and carriage return
-  }
+  Serial.print("{dischargeTime: \"");
+  Serial.print(String(elapsedTime));
+  Serial.print("\", capacity: \"");
+  Serial.print(String(picoFarads));
+  Serial.println("\"}");
 
   /* dicharge the capacitor  */
   digitalWrite(chargePin, LOW);             // set charge pin to  LOW 
